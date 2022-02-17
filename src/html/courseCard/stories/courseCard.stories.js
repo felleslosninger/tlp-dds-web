2f80b9f7a9de4c4f5b912991160052ff897b2232
Bendik Kvernevik <bendiksem93@gmail.com>
2022-02-17 16:55:36 +0100

    docs(storybook): add course-card story

---
+++ src/html/courseCard/stories/courseCard.stories.js
@@ -0,0 +1,24 @@
+import { createCourseCard } from '../courseCard.js'
+import { withDesign } from 'storybook-addon-designs'
+
+export default {
+  title: 'Komponenter/Course-card',
+  decorators: [withDesign],
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/rrBqUrSQl6jdAJpjUzV3cN/?node-id=1025%3A4412',
+    },
+    backgrounds: {
+      default: 'grey-200',
+    },
+  },
+  argTypes: {},
+}
+
+const Template = (args) => {
+  return createCourseCard(args)
+}
+
+export const Default = Template.bind({})
+Default.args = {}
