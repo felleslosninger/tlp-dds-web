c2066d8d6432e803ee37b039b8a2c59811728744
Karsten Bjelde <fotball-karsten@hotmail.com>
2022-04-01 11:49:12 +0200

    feat(navigationCard): added navigationCard js file

---
+++ src/html/navigationCard/navigationCard.js
@@ -0,0 +1,11 @@
+const createNavigationCard = ({}) => {
+  const classPrefix = 'ddsweb-navigation-card'
+
+  // Element content
+
+  return String.raw`
+
+    `
+}
+
+export { createNavigationCard }
