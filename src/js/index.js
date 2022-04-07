3d71d3b42d99841f8a158fd82d550fb3a431b2d3
Karsten Bjelde <fotball-karsten@hotmail.com>
2022-04-07 10:40:55 +0200

    feat(navigationcard): fixed formatting errors

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 import jQuery from 'jquery'
 
 // Import component javascript modules
-import {dropdown} from "./_dropdown";
-import {headerDrawers} from "./_header-drawers";
+import { dropdown } from './_dropdown'
+import { headerDrawers } from './_header-drawers'
 
 // Set jQuery globally
 window.$ = window.jQuery = jQuery
 
 // Run ES6 modules
-dropdown();
-headerDrawers();
+dropdown()
+headerDrawers()
