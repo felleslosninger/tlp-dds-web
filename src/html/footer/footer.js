50d7098b53de5f74e118005fc0311d5962980708
Karsten Bjelde <fotball-karsten@hotmail.com>
2022-04-11 14:31:17 +0200

    footer: Fått på plass innholdet, manglar twitter og logo ikonet. Spacing må på plass og

--- src/html/footer/footer.js
+++ src/html/footer/footer.js
@@ -1,3 +1,6 @@
+import chevronRightIcon from '@digdir/ds-icons/svg/chevron_right/outline.svg'
+import facebook from '@digdir/ds-icons/svg/facebook/outline.svg'
+
 const footer = () => {
   const classPrefix = 'ddsweb-footer'
 
@@ -9,11 +12,30 @@ const footer = () => {
   // Elements
 
   return String.raw`
-    <footer>
-    <div class=${classPrefix}>
-    <div class='${classPrefix}__left'><h1>left</h1></div>
-    <div class='${classPrefix}__middle'><h1>middle</h1></div>
-    <div class='${classPrefix}__right'><h1>Right</h1></div>
+    <footer class="container">
+    <div class='${classPrefix} row'>
+    <div class='${classPrefix}__left col-lg-4 col-md-5 col-xs-3'>
+    <h1 class='${classPrefix}__title'><span>Logo</span> Digdir </h1>
+    <p class='${classPrefix}__paragraph'>${descriptionText}</p> <span>${facebook}</span><span>logo</span></div>
+    <div class='${classPrefix}__middle col-lg-4 col-md-5 col-xs-3'>
+    <h1 class='${classPrefix}__title'>${titleText}</h1>
+    <ul class='${classPrefix}__list'>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Arkitekturprinsipper</li></a>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Deling av data</li></a>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Informasjonsforvaltning</li></a>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Design</li></a>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Interne applikasjoner</li></a>
+    </ul>
+    </div>
+    <div class='${classPrefix}__right col-lg-4 col-md-5 col-xs-3'><h1 class='${classPrefix}__title'>${titleText}</h1>
+    <ul class='${classPrefix}__list'>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Arkitekturprinsipper</li></a>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Deling av data</li></a>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Informasjonsforvaltning</li></a>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Design</li></a>
+    <a class='${classPrefix}__list--link' href=# ><li class='${classPrefix}__list--item'>${chevronRightIcon}Interne applikasjoner</li></a>
+    </ul>
+    </div>
     </div>
     </footer>
     `
