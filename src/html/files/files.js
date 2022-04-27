ed5e9d15cb363ac6ae892ef470d49dc80ce11cf2
Karsten Bjelde <fotball-karsten@hotmail.com>
2022-04-27 10:27:02 +0200

    feat(files): desktop version is finished

---
+++ src/html/files/files.js
@@ -0,0 +1,58 @@
+import ImageIcon from '@digdir/ds-icons/svg/outline/Image.svg'
+import DownloadIcon from '@digdir/ds-icons/svg/outline/Download.svg'
+
+const createFiles = ({ backgroundColor = 'blue' }) => {
+  const classPrefix = 'ddsweb-files'
+
+  // Element content
+  const titleText = 'Dokument tittel'
+
+  const paragraphText = `Kort tekst knyttet til dokumentet
+  `
+  // Functions
+
+  const changeBackground = () => {
+    switch (backgroundColor) {
+      case 'blue':
+        return classPrefix + '--blue'
+      case 'yellow':
+        return classPrefix + '--yellow'
+      case 'red':
+        return classPrefix + '--red'
+      default:
+        return classPrefix + ''
+    }
+  }
+
+  const desktopHtml = `
+  <a class='${classPrefix} ${changeBackground()}' href="#">
+<div class='${classPrefix}__icon--left'>
+<span class='${classPrefix}__icon--image'>${ImageIcon}</span>
+</div>
+<div class='${classPrefix}__content'>
+<h1 class='${classPrefix}__title'>${titleText}</h1>
+<p class='${classPrefix}__paragraph'>${paragraphText}</p>
+</div>
+<div class='${classPrefix}__icon--right'>
+<span class='${classPrefix}__icon--download'>${DownloadIcon}</span>
+</div>
+</a>`
+
+  const mobileHtml = `
+<a class='${classPrefix}' href="#">
+<span class='${classPrefix}__icon--image'>${ImageIcon}</span>
+<span class='${classPrefix}__icon--download'>${DownloadIcon}</span>
+<div class='${classPrefix}__content'>
+<h1 class='${classPrefix}__title'>${titleText}</h1>
+<p class='${classPrefix}__paragraph'>${paragraphText}</p>
+
+</div>
+</a>
+`
+
+  return String.raw`
+     ${desktopHtml}
+    `
+}
+
+export { createFiles }
