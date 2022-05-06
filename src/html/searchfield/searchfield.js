363d81f7928bd9c6cc229ee32dec7b0c9d5d0c79
Karsten Bjelde <fotball-karsten@hotmail.com>
2022-05-06 09:40:51 +0200

    feat(search-field) the design is almost in place

---
+++ src/html/searchfield/searchfield.js
@@ -0,0 +1,25 @@
+import searchIcon from '@digdir/ds-icons/svg/outline/Search.svg'
+
+const createSearchField = ({}) => {
+  const classPrefix = 'ddsweb-search-field'
+
+  // Element content
+  const placeHolderText = 'Søk her ...'
+  const buttonText = 'Søk'
+
+  // Functions
+
+  // Elements
+
+  return String.raw`
+  <div class="${classPrefix}">
+  <span aria-hidden="true" class="${classPrefix}__icon"> ${searchIcon}</span>
+  <input class="${classPrefix}__input" type="text" placeholder="${placeHolderText}"  />
+  <button type="button" class="${classPrefix}__button">${buttonText}</button>
+
+  </div>
+      
+          `
+}
+
+export { createSearchField }
