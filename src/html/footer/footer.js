e828d506e9b5f6138979ad6f936b9702bb3e7067
Karsten Bjelde <fotball-karsten@hotmail.com>
2022-05-09 09:58:54 +0200

    feat(footer): cleaned up code

--- src/html/footer/footer.js
+++ src/html/footer/footer.js
@@ -12,14 +12,53 @@ const footer = () => {
     'Embarrassed space windshield children. The detailed however, how provide earnestly been and for he a physics by the didn`t were evening.'
 
   // Elements
+  // functions
+
+  const list1 = [
+    {
+      name: 'Arkitekturprinsipper',
+      url: '#',
+    },
+    {
+      name: 'Deling av data',
+      url: '#',
+    },
+    {
+      name: 'Informasjonsforvaltning',
+      url: '#',
+    },
+    {
+      name: 'Design',
+      url: '#',
+    },
+    {
+      name: 'Interne applikasjoner',
+      url: '#',
+    },
+  ]
+
+  const list = (array) => {
+    return String.raw`
+   <ul class='${classPrefix}__list'>
+   ${array.reduce(
+     (updated, latest) =>
+       updated.concat(
+         ` <li class="${classPrefix}__list-item"><a class="${classPrefix}__list-link" href='${latest.url}'>${ChevronRightIcon}${latest.name}</a></li>`,
+       ),
+     '',
+   )}
+  </ul>
+   
+   `
+  }
 
   //language=HTML
   return String.raw`
-        <footer>
+        <footer class='${classPrefix}'>
             <div class='container'>
-                <div class='${classPrefix} row justify-content-between'>
+                <div class='row justify-content-center'>
 
-                    <div class='${classPrefix}__left col-xl-4  col-md-5 col-xs-10'>
+                    <div class='col-xl-4 col-md-5 col-xs-10'>
                         <div class='${classPrefix}__logo--icon'>${DigdirIcon}</div>
 
                         <p class='${classPrefix}__paragraph'>${descriptionText}</p>
@@ -32,39 +71,19 @@ const footer = () => {
                             </a>
                         </div>
                     </div>
-                    <div class='${classPrefix}__middle col-xl-3  col-md-5 col-xs-3'>
+                    <div class='col-xl-3 col-md-5 col-xs-3'>
                         <div class='${classPrefix}__title'>Fellesløsninger</div>
                         <ul class='${classPrefix}__list'>
-                            <a class='${classPrefix}__list--link' href=#>
-                                <li class='${classPrefix}__list--item'>${ChevronRightIcon}Arkitekturprinsipper</li>
-                            </a>
-                            <a class='${classPrefix}__list--link' href=#>
-                                <li class='${classPrefix}__list--item'>${ChevronRightIcon}Deling av data</li>
-                            </a>
-                            <a class='${classPrefix}__list--link' href=#>
-                                <li class='${classPrefix}__list--item'>${ChevronRightIcon}Informasjonsforvaltning</li>
-                            </a>
-                            <a class='${classPrefix}__list--link' href=#>
-                                <li class='${classPrefix}__list--item'>${ChevronRightIcon}Design</li>
-                            </a>
+                        ${list(list1)}
                         </ul>
+                        
+                        
                     </div>
 
-                    <div class='${classPrefix}__right col-xl-4  col-md-10 col-xs-3'>
+                    <div class='col-xl-4 col-md-10 col-xs-3'>
                       <div class='${classPrefix}__title'>Jobbe med digitalisering</div>
                         <ul class='${classPrefix}__list'>
-                            <a class='${classPrefix}__list--link' href=#>
-                                <li class='${classPrefix}__list--item'>${ChevronRightIcon}Digitale tjenester</li>
-                            </a>
-                            <a class='${classPrefix}__list--link' href=#>
-                                <li class='${classPrefix}__list--item'>${ChevronRightIcon}Deling av data</li>
-                            </a>
-                            <a class='${classPrefix}__list--link' href=#>
-                                <li class='${classPrefix}__list--item'>${ChevronRightIcon}Sikkerhet og utvikling</li>
-                            </a>
-                            <a class='${classPrefix}__list--link' href=#>
-                                <li class='${classPrefix}__list--item'>${ChevronRightIcon}Design</li>
-                            </a>
+                         ${list(list1)}
                         </ul>
                     </div>
                 </div>
