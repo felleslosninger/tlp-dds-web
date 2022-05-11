58330244d6c186769bc304c7d952c6d9729e43d0
Karsten Bjelde <fotball-karsten@hotmail.com>
2022-05-11 14:22:20 +0200

    feat(header): Styled, and wrote jquery to change between icons on the mnu btn

--- src/html/header/header.js
+++ src/html/header/header.js
@@ -1,12 +1,51 @@
 import searchIcon from '@digdir/ds-icons/svg/outline/Search.svg'
 import menuIcon from '@digdir/ds-icons/svg/outline/Menu.svg'
+import closeIcon from '@digdir/ds-icons/svg/outline/Close.svg'
 import { createSearchField } from '../searchfield/searchfield'
+
+
 
 const classPrefix = 'ddsweb-header'
 const menuClassPrefix = 'ddsweb-menu'
 const dropdownClassPrefix = 'ddsweb-dropdown'
+const classPrefixMegaMenu = 'ddsweb-header-mega-menu'
+const classPrefixIconLink = 'dds-icon-link'
+const croppedChevronSvg = String.raw`
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="8.59 6 7.41 12">
+        <path d="M10 6L8.59 7.41L13.17 12l-4.58 4.59L10 18l6-6l-6-6z" />
+    </svg>`
+
+const shortcutList = (list) => {
+    return String.raw`
+        <li class="${classPrefixMegaMenu}__item">
+            <a class="${classPrefixMegaMenu}__list-title" href="#">Jobbe med digitalisering</a>
+            <ul class="${classPrefixMegaMenu}__nested-list">
+                ${list.map(item => `<li class="${classPrefixMegaMenu}__nested-item">${shortcutLink(item)}</li>`).join("")}
+            </ul>
+        </li>
+    `
+}
+
+//language=HTML
+const shortcutLink = (text) => {
+    return String.raw`
+        <a class="${classPrefixIconLink} ${classPrefixIconLink}--shortcut ${classPrefixIconLink}--icon-left"
+            href="https://www.digdir.no/" rel="noreferrer noopener">
+            ${croppedChevronSvg}
+            ${text}
+        </a>
+    `
+}
+
+
+
+
+
+
 
 const createHeader = ({ }) => {
+
+
     return String.raw`
 
 <header class="${classPrefix}">
@@ -21,8 +60,8 @@ const createHeader = ({ }) => {
                 <ul class="${menuClassPrefix}">
                     <li class="${menuClassPrefix}__item">
                         <div class="${dropdownClassPrefix}">
-                            <button class="${dropdownClassPrefix}__toggle dropdown-toggle" type="button"
-                                id="dropdownMenuButton" aria-haspopup="true" data-dds-toggle="#tema"
+                            <button class="${dropdownClassPrefix}__theme ${dropdownClassPrefix}__toggle dropdown-toggle"
+                                type="button" id="dropdownMenuButton" aria-haspopup="true" data-dds-toggle="#tema"
                                 aria-expanded="false" data-bs-toggle="dropdown"> Tema</button>
 
                             <ul class="${dropdownClassPrefix}__list dropdown-menu" aria-labelledby="dropdownMenuButton">
@@ -50,8 +89,10 @@ const createHeader = ({ }) => {
             </div>
             <div class="${classPrefix}__top-bar-right">
                 <div class="${classPrefix}__search">
-                    <button class="${classPrefix}__search-button" data-toggle="#search"
-                        aria-expanded="false">${searchIcon}Søk</button>
+                    <button class="${classPrefix}__search-button ${classPrefix}__toggle" data-toggle="#search"
+                        aria-expanded="false">
+                        <span class="${classPrefix}__search-icon" data-toggle="#search">${searchIcon
+                           }</span>Søk</button>
 
                 </div>
                 <div class="${classPrefix}__expanded" id="search">
@@ -60,8 +101,36 @@ const createHeader = ({ }) => {
                         ${createSearchField({})}
                     </div>
                 </div>
-                <button class="${classPrefix}__menu-button"><span
-                        class="${classPrefix}__menu-icon">${menuIcon}</span>Meny</button>
+                <button class="${classPrefix}__menu-button ${classPrefix}__toggle" data-toggle="#menu"
+                    aria-expanded="false">
+                    <span class="${classPrefix}__menu-icon" data-toggle="#menu">${menuIcon}</span>Meny
+                </button>
+            </div>
+        </div>
+        <div class="${classPrefix}__expanded" id="menu">
+            <div class="${classPrefix}__expanded-content">
+                <div class="${classPrefix}-mega-menu">
+                    <h2 class="${classPrefixMegaMenu}__title">Temaoversikt</h2>
+                    <ul class="${classPrefixMegaMenu}__list">
+                        ${shortcutList(['Arkitekturprinsipper', 'Deling av data', 'Informasjonsforvaltning', 'Design',
+                        'Interne applikasjoner', 'Fellesløsninger'])}
+                        ${shortcutList(['Arkitekturprinsipper', 'Deling av data', 'Informasjonsforvaltning', 'Design',
+                        'Interne applikasjoner', 'Fellesløsninger'])}
+                        ${shortcutList(['Arkitekturprinsipper', 'Deling av data', 'Informasjonsforvaltning', 'Design',
+                        'Interne applikasjoner', 'Fellesløsninger'])}
+                        ${shortcutList(['Arkitekturprinsipper', 'Deling av data', 'Informasjonsforvaltning', 'Design',
+                        'Interne applikasjoner', 'Fellesløsninger'])}
+
+                        ${shortcutList(['Arkitekturprinsipper', 'Deling av data', 'Informasjonsforvaltning', 'Design',
+                        'Interne applikasjoner', 'Fellesløsninger'])}
+                        ${shortcutList(['Arkitekturprinsipper', 'Deling av data', 'Informasjonsforvaltning', 'Design',
+                        'Interne applikasjoner', 'Fellesløsninger'])}
+                        ${shortcutList(['Arkitekturprinsipper', 'Deling av data', 'Informasjonsforvaltning', 'Design',
+                        'Interne applikasjoner', 'Fellesløsninger'])}
+                        ${shortcutList(['Arkitekturprinsipper', 'Deling av data', 'Informasjonsforvaltning', 'Design',
+                        'Interne applikasjoner', 'Fellesløsninger'])}
+                    </ul>
+                </div>
             </div>
         </div>
     </div>
@@ -71,13 +140,12 @@ const createHeader = ({ }) => {
             <div class="${classPrefix}__bottom-bar-right"></div>
         </div>
     </div>
-
-
-
+    <div class="${classPrefix}__overlay"></div>
 </header>
 
 
 `
+
 }
 
 export default createHeader
--- src/js/_header-drawers.js
+++ src/js/_header-drawers.js
@@ -1,3 +1,6 @@
+import closeIcon from '@digdir/ds-icons/svg/outline/Close.svg'
+import menuIcon from '@digdir/ds-icons/svg/outline/Menu.svg'
+
 export function headerDrawers() {
   $(document).ready(function () {
     const toggleBtns = $('button[data-toggle]')
@@ -8,11 +11,10 @@ export function headerDrawers() {
       let target = $(e.target)
       let dropdownSelector = target.data('toggle')
       let dropdown = $(dropdownSelector)
-
       closeOtherDropdowns(dropdown)
-
       if (dropdown.hasClass('dds-dropdown__active')) {
         closeDropdown(dropdown, true)
+
       } else {
         openDropdown(dropdown)
       }
@@ -30,9 +32,19 @@ export function headerDrawers() {
     function openDropdown(dropdown) {
       let dropdownContent = dropdown.children().first()
       let dropdownHeight = dropdownContent.outerHeight()
+      let menuBtn = $('.ddsweb-header__menu-icon')
+      let menu = $('#menu')
+      let btnText = $('.ddsweb-header__menu-button')
       dropdown.prev().attr('aria-expanded', true)
-
       dropdown.addClass('dds-dropdown__active')
+      if (menu.hasClass('dds-dropdown__active')) {
+
+        menuBtn.html(closeIcon)
+        btnText.html(`<span class="ddsweb-header__menu-icon" data-toggle="#menu">${closeIcon}</span> Lukk`)
+      } else {
+        menuBtn.html(menuIcon)
+        btnText.html(`<span class="ddsweb-header__menu-icon" data-toggle="#menu">${menuIcon}</span> Meny`)
+      }
 
       overlay.fadeIn(animationSpeed)
       dropdown.animate(
@@ -50,11 +62,24 @@ export function headerDrawers() {
       )
     }
 
+
+
     function closeDropdown(dropdown, anim) {
       let dropdownContent = dropdown.children().first()
+      let menuBtn = $('.ddsweb-header__menu-icon')
+      let menu = $('#menu')
+      let btnText = $('.ddsweb-header__menu-button')
       dropdown.prev().attr('aria-expanded', false)
 
       dropdown.removeClass('dds-dropdown__active')
+      console.log(dropdown);
+      if (!menu.hasClass('dds-dropdown__active')) {
+        menuBtn.html(menuIcon)
+        btnText.html(`<span class="ddsweb-header__menu-icon" data-toggle="#menu">${menuIcon}</span> Meny`)
+      } else {
+        menuBtn.html(closeIcon)
+        btnText.html(`<span class="ddsweb-header__menu-icon" data-toggle="#menu">${closeIcon}</span> Lukk`)
+      }
 
       if (anim) {
         overlay.fadeOut(animationSpeed)
